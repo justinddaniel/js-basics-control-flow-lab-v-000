f8fd98a89642d8740d244fd7c7761d48a332f12a
Justin Daniel <justinddaniel@gmail.com>
2018-01-12 21:55:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+let scuberGreetingForFeet(distance)
+
+if (distance <= 400) {
+  scuberGreetingForFeet = 'This one is on me!'
+} else if (distance > 2000 && distance <= 2500) {
+  scuberGreetingForFeet = 'I will gladly take your third bucks.'
+} else if (distance > 2500) {
+  scuberGreetingForFeet = 'No can do.'
+}
+
