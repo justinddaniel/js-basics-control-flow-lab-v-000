a1f44f179de1503c5033843cbb358fcb16778d73
Justin Daniel <justinddaniel@gmail.com>
2018-01-12 22:16:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 function scuberGreetingForFeet (distance) {
   if (distance <= 400) {
-    return 'This one is on me!'
+    return 'This one is on me!';
   } else if (distance > 2000 && distance <= 2500) {
-    return 'I will gladly take your third bucks.'
+    return 'I will gladly take your third bucks.';
   } else if (distance > 2500) {
-    return = 'No can do.'
+    return = 'No can do.';
   }
 }
