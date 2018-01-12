e793efab97e906602e2eaeac8bb781910b246bea
Justin Daniel <justinddaniel@gmail.com>
2018-01-12 22:37:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ function ternaryCheckCity (city) {
 }
 
 
-function switchOnCharmFromTip (charm) {
-  switch (charm) {
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
     case 'generous':
       return "Thank you so much.";
     case 'not as generous':
       return "Thank you.";
       break;
-    default: 
+    default:
       return "Bye."
   }
 }
